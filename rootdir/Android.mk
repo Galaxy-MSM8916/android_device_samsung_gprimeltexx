628352c1e6cbe6c21e43a498e976f0cc7da88190
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-08-18 01:25:51 -0400

    Added qcom radio init script

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -194,6 +194,16 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 ######################
+### init.qcom.fm.sh
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.fm.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+######################
 ### init.rilcommon.rc
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.rilcommon.rc
