099ce1bcc0243d6a74829fb5766c4414deae3213
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-03-09 16:06:39 -0500

    gprimelte: Remove redundant AOSP Makefiles

--- AndroidProducts.mk
+++
@@ -1,3 +0,0 @@
-LOCAL_PATH := device/samsung/gprimeltexx
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_gprimeltexx.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
 for i in eng userdebug user; do
-#	add_lunch_combo omni_gprimeltexx-${i}
-	add_lunch_combo cm_gprimeltexx-${i}
+	add_lunch_combo lineage_gprimeltexx-${i}
 done
