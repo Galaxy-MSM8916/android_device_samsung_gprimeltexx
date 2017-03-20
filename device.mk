c44c09fb5c3b70606edc9a35b9c8c63155d3032d
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-03-20 02:39:52 -0400

    gprimelte: Move acdbs to vendor tree

--- device.mk
+++ device.mk
@@ -11,13 +11,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Configurations
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/audio/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
-	$(LOCAL_PATH)/audio/General_cal.acdb:system/etc/General_cal.acdb \
-	$(LOCAL_PATH)/audio/Global_cal.acdb:system/etc/Global_cal.acdb \
-	$(LOCAL_PATH)/audio/Handset_cal.acdb:system/etc/Handset_cal.acdb \
-	$(LOCAL_PATH)/audio/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
-	$(LOCAL_PATH)/audio/Headset_cal.acdb:system/etc/Headset_cal.acdb \
-	$(LOCAL_PATH)/audio/Speaker_cal.acdb:system/etc/Speaker_cal.acdb \
 	$(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
 	$(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
 	$(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
