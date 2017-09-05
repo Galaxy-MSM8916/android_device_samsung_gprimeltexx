a312e5a9c4f1bee4969e9f54b502a1b5ec5a94e4
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-09-05 07:57:14 -0400

    gprimeltexx: Move system properties to system.prop
    Change-Id: Iceb07b42b84838958edc6508dcd519906b140d8c

--- device.mk
+++ device.mk
@@ -6,9 +6,6 @@ $(call inherit-product, device/samsung/gprimelte-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/gprimeltexx
 
-# System properties
--include $(LOCAL_PATH)/system_prop.mk
-
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
